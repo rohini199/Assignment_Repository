b9724832174456b8cd483b02eb92d602b3b05ae3
Rohini_Immanni <Rohini_Immanni@HYDHTC354133D.ad.infosys.com>
2017-05-15 10:42:24 +0530

    Selected a date for June in BookTicket class

--- Assignment/src/PackageAssignment/BookTicket.java
+++ Assignment/src/PackageAssignment/BookTicket.java
@@ -5,6 +5,7 @@ public class BookTicket {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Select the date for booking ticket");
+System.out.println("Selected a date for June");
 	}
 
 }
