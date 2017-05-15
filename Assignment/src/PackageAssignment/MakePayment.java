f745b634b48b294e4bc2534e55c7443592bcd332
Rohini_Immanni <Rohini_Immanni@HYDHTC354133D.ad.infosys.com>
2017-05-15 10:44:43 +0530

    Added message in MakePayment Class

--- Assignment/src/PackageAssignment/MakePayment.java
+++ Assignment/src/PackageAssignment/MakePayment.java
@@ -5,6 +5,7 @@ public class MakePayment {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Please enter the card details");
+System.out.println("Entering invalid card details");
 	}
 
 }
